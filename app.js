8a40d76f74cb61d0d708ff66d134710f55ecc67c
Mudassir Malik <Malikjrw147@gmail.com>
2022-09-12 13:07:43 +0500

    database connection removed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const mongoURI = `mongodb://127.0.0.1:27017/CHAT`
-mongoose.connect(mongoURI, { useNewUrlParser: true })
-    .then(() => console.log('Mongodb connected'))
-    .catch(err => console.log('Erro during mongodb connection :', err))
+// const mongoose = require('mongoose');
+// const mongoURI = `mongodb://127.0.0.1:27017/CHAT`
+// mongoose.connect(mongoURI, { useNewUrlParser: true })
+//     .then(() => console.log('Mongodb connected'))
+//     .catch(err => console.log('Erro during mongodb connection :', err))
 //Middlewares
 app.use(express.static('build'))
 app.use('/adeel', (req, res) => {
